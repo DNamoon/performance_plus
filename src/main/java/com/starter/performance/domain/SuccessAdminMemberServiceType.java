a81765e987ef83c99303eb4b27a1c9380f96a56b
EJ <daiana_@naver.com>
2023-09-03 19:46:13 +0900

    fix: AdminMemberService 관련 코드 전면 수정

---
+++ src/main/java/com/starter/performance/domain/SuccessAdminMemberServiceType.java
@@ -0,0 +1,9 @@
+package com.starter.performance.domain;
+
+public enum SuccessAdminMemberServiceType {
+
+  SUCCESS_INQUIRY_MEMBER_MESSAGE,
+  SUCCESS_INQUIRY_MEMBER_ALL_MESSAGE,
+  SUCCESS_FIND_MEMBER_ALL_MESSAGE,
+  SUCCESS_BLOCK_MEMBER_MESSAGE;
+}
