7e4f293981fa515d8da3c50777819c6ff38970e9
ming <bamormiog@naver.com>
2023-09-08 17:11:45 +0900

    fix: 충돌 해결

--- src/main/java/com/starter/performance/domain/Member.java
+++ src/main/java/com/starter/performance/domain/Member.java
@@ -16,6 +16,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
