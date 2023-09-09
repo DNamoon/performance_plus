485943482b6de62db5e1dfd47c2a2bae6f72d323
EJ <daiana_@naver.com>
2023-09-09 22:50:28 +0900

    refactor: 누락 파일 추가

---
+++ src/main/java/com/starter/performance/domain/Rating.java
@@ -0,0 +1,28 @@
+package com.starter.performance.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Rating {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  @NotNull
+  private String name;
+}
