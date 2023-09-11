b871f6d91c53ccccb69a6707852b298d4289f751
MH <gmhmhmhg.dev@gmail.com>
2023-09-12 03:40:50 +0900

    refactor : bookmarkList

--- src/main/java/com/starter/performance/service/dto/BookmarkResponseDto.java
+++ src/main/java/com/starter/performance/service/dto/BookmarkResponseDto.java
@@ -1,11 +1,13 @@
 package com.starter.performance.service.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 @Getter
 @Builder
+@AllArgsConstructor
 public class BookmarkResponseDto {
     private String performanceName;
     private LocalDateTime performanceDate;
