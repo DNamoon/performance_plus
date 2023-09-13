67f0844746b0a89d295a744ec77706701cea404e
MH <gmhmhmhg.dev@gmail.com>
2023-09-13 17:45:54 +0900

    fix : 컬럼 조정으로 인한 북마크 생성 수정

--- src/main/java/com/starter/performance/repository/PerformanceRepository.java
+++ src/main/java/com/starter/performance/repository/PerformanceRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface PerformanceRepository extends JpaRepository<Performance, Long>, PerformanceRepositoryQueryDsl {
-    Optional<Performance> findByIdAndName(Long Id, String name);
-    Optional<Performance> findById(Long id);
+        Optional<Performance> findById(Long id);
 }
